8c423e95b9c74bd261eabba7a7ee2cb6ebe4678c
hawa ebrahim <hawaebrahimhamid@gmail.com>
2025-12-03 00:49:30 +0800

    add fiel

--- js/script.js
+++ js/script.js
@@ -14,7 +14,103 @@ snake[0] = {
   y: Math.floor(Math.random() * columns) * scale,
 };
 
-ctx.fillStyle = "#fff";
-ctx.strokeStyle = "pink";
-ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
-ctx.strokeRect(snake[0].x, snake[0].y, scale, scale);
+let food = {
+  x: Math.floor(Math.random() * columns) * scale,
+  y: Math.floor(Math.random() * columns) * scale,
+};
+
+//  ctx.fillStyle = "#fff";
+// ctx.strokeStyle = "pink";
+// ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
+// ctx.strokeRect(snake[0].x, snake[0].y, scale, scale);
+
+// direction
+let d = "right";
+
+document.onkeydown = direction;
+function direction(event) {
+  let key = event.keyCode;
+  if (key == 37 && d != "right") {
+    d = "left";
+  } else if (key == 38 && d != "down") {
+    d = "up";
+  } else if (key == 39 && d != "left") {
+    d = "right";
+  } else if (key == 40 && d != "up") {
+    d = "down";
+  }
+}
+
+// call our draw function every 100 ms
+let playGame = setInterval(draw, 100);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // draw snake
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = "#fff";
+    ctx.strokeStyle = "red";
+    ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
+    ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
+  }
+  // draw food
+  ctx.fillStyle = "#ff0";
+  ctx.strokeStyle = "green";
+  ctx.fillRect(food.x, food.y, scale, scale);
+  ctx.strokeRect(food.x, food.y, scale, scale);
+
+  //  old head position
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  // which direction
+  if (d == "left") snakeX -= scale;
+  if (d == "up") snakeY -= scale;
+  if (d == "right") snakeX += scale;
+  if (d == "down") snakeY += scale;
+
+  if (snakeX > canvas.width) {
+    snakeX = 0;
+  }
+  if (snakeY > canvas.height) {
+    snakeY = 0;
+  }
+  if (snakeX < 0) {
+    snakeX = canvas.width;
+  }
+  if (snakeY < 0) {
+    snakeY = canvas.height;
+  }
+
+  // if the snake eats the food, it grows
+  if (snakeX == food.x && snakeY == food.y) {
+    food = {
+      x: Math.floor(Math.random() * columns) * scale,
+      y: Math.floor(Math.random() * columns) * scale,
+    };
+    //     // we don't remove the tail
+  } else {
+    // remove the tail
+    snake.pop();
+  }
+  // console.log(snake);
+  //  new head position
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+  // console.log(snake);
+  if (eatSelf(newHead, snake)) {
+    clearInterval(playGame);
+  }
+
+  snake.unshift(newHead);
+}
+// check if snake is eating it self
+function eatSelf(head, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
+    }
+  }
+  return false;
+}
